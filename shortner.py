b62c32129bccc8e94351aeb37b4e1366eb81aff6
Luis <86352845+LuisEduard-o@users.noreply.github.com>
2026-02-11 14:47:06 -0300

    Fix HTML links in shortner.py

--- shortner.py
+++ shortner.py
@@ -413,8 +413,8 @@ async function carregarLista() {
       <td>${totalHits}</td>
       <td class="row">
         <button class="btn" onclick="copiar('${location.origin}/${code}')">Copiar</button>
-        /${code}Abrir</a>
-        /stats/${code}Stats</a>
+        <a href="/${code}" target="_blank">Abrir</a>
+        <a href="/stats/${code}" target="_blank">Stats</a>
         <button class="btn" onclick="abrirEdicao('${code}')">Editar</button>
         <button class="btn-danger" onclick="excluirLink('${code}')">Excluir</button>
       </td>
